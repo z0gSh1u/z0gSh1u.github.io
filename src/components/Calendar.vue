43c812d0001440d9f39d93b6b5c52e4f73160f87
z0gSh1u <136371391@qq.com>
2021-12-12 13:09:08 +0800

    Responsive calendar.

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,13 +1,21 @@
 <template>
   <div id="calendar">
-    <iframe
-      src="https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%237986CB&ctz=Asia%2FShanghai&showTitle=1&title=Public%20Calendar%20of%20Xu%20Zhuo&mode=MONTH&showNav=1&showPrint=0&showTabs=1&showCalendars=0&src=YzExMTkwMkBnbWFpbC5jb20&color=%234285F4"
-      style="border-width:0"
-      width="800"
-      height="600"
-      frameborder="0"
-      scrolling="no"
-    ></iframe>
+    <div class="responsive-iframe-container big-container">
+      <iframe
+        src="https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%23A0D8EF&ctz=Asia%2FShanghai&showTitle=1&title=Public%20Calendar%20of%20Xu%20Zhuo&mode=MONTH&showNav=1&showPrint=0&showTabs=1&showCalendars=0&src=YzExMTkwMkBnbWFpbC5jb20&color=%234285F4"
+        style="border-width:0"
+        frameborder="0"
+        scrolling="no"
+      ></iframe>
+    </div>
+    <div class="responsive-iframe-container small-container">
+      <iframe
+        src="https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%23A0D8EF&ctz=Asia%2FShanghai&showTitle=1&title=Public%20Calendar%20of%20Xu%20Zhuo&mode=AGENDA&showNav=1&showPrint=0&showTabs=1&showCalendars=0&src=YzExMTkwMkBnbWFpbC5jb20&color=%234285F4"
+        style="border-width:0"
+        frameborder="0"
+        scrolling="no"
+      ></iframe>
+    </div>
     <p class="calendar-hint">本页面使用 Google 服务，如无法正常显示日历，请检查与 Google 的连接。</p>
   </div>
 </template>
@@ -27,7 +35,37 @@ export default class Calendar extends Vue {}
 .calendar-hint {
   font-size: 0.5rem;
   text-align: right;
-  margin-right: 1rem;
+  margin-right: 0.25rem;
   margin-top: 1rem;
+}
+@media (max-width: 720px) {
+  .big-container {
+    display: none;
+  }
+}
+@media (min-width: 720px) {
+  .small-container {
+    display: none;
+  }
+}
+/* Responsive iFrame */
+/* https://stackoverflow.com/questions/5925629/ */
+.responsive-iframe-container {
+  position: relative;
+  padding-bottom: 56.25%;
+  padding-top: 0px;
+  height: 0;
+  width: 95%;
+  margin: 0 auto;
+  overflow: hidden;
+}
+.responsive-iframe-container iframe,
+.responsive-iframe-container object,
+.responsive-iframe-container embed {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
 }
 </style>
