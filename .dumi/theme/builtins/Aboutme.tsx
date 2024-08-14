3c8b7641a249e005db72c49b4a51e369afa226c2
z0gSh1u <zx.cs@qq.com>
2024-08-15 00:31:36 +0800

    patch dumi theme-default.

--- .dumi/theme/builtins/Aboutme.tsx
+++ .dumi/theme/builtins/Aboutme.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Row, Col, Avatar } from 'antd';
 import DumiP from './_DumiP';
 import styled from 'styled-components';
-import { useLocation } from 'dumi';
 
 const MyName = styled.p`
   font-size: 2em;
@@ -21,34 +20,16 @@ const DescriptionZHCN = (
     <a href="https://xtech.antfin.com/" target="_blank">
       体验技术部
     </a>
-    从事前端工程师工作，
-  </DumiP>
-);
-
-const DescriptionENUS = (
-  <DumiP>
-    Currently I am working
-    <a href="https://www.antgroup.com/" target="_blank">
-      蚂蚁集团
-    </a>
-    的
-    <a href="https://xtech.antfin.com/" target="_blank">
-      体验技术部
-    </a>
-    从事前端工程师工作，
+    从事前端工程师工作，并在向全栈工程师方向发展。
   </DumiP>
 );
 
 export default function Aboutme() {
-  const location = useLocation();
-
   return (
     <Row style={{ marginBottom: '2em', marginTop: '1em' }} gutter={16}>
       <Col span={16}>
         <MyName>卓旭</MyName>
-        {location.pathname.includes('en-US')
-          ? DescriptionENUS
-          : DescriptionZHCN}
+        {DescriptionZHCN}
       </Col>
       <Col span={8} style={{ textAlign: 'right' }}>
         <Avatar size={192} src={'/photo.jpg'}></Avatar>
--- .dumi/typing.d.ts
+++ .dumi/typing.d.ts
@@ -2,3 +2,13 @@ declare module "*.less" {
   const content: { [className: string]: any };
   export default content;
 }
+
+declare module '*.svg' {
+  import * as React from 'react';
+  export const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement> & { title?: string }
+  >;
+
+  const src: string;
+  export default src;
+}
